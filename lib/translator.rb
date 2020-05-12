160a361569733f457b991a44a2385ef97ad0650e
Melody Soriano <mel.soriano6123@gmail.com>
2020-05-12 01:26:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,20 @@
 # require modules here
-
-def load_library
+require 'yaml'
+def load_library(path)
   # code goes here
+  emoticons = YAML.load(File.read("#{path}"))
+  better_emoticons = {}
+  emoticons.each {|key, values|
+    better_emoticons[key] = {}
+    better_emoticons[key][:english] = values[0]
+    better_emoticons[key][:japanese] = values[1]
+  }
 end
 
 def get_japanese_emoticon
   # code goes here
+  emoticons = load_library
+
 end
 
 def get_english_meaning
